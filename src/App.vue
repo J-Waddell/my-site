f76a66f8a567b108c27021996886ae6f0b932074
Jesse Waddell <waddelljesse1@gmail.com>
2023-08-17 18:48:45 -0500

    Created Repo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img id="homeFace" alt="Vue logo" src="./assets/Jesse_Waddell.jpg">
+  <HelloWorld msg="I'm here to punch ya baby"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#homeFace {
+  width: 25%;
+  border-radius: 3em;
+}
+</style>
